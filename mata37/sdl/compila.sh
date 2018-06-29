219cc9aad13a2b8087ef2acea53c86d9ec36b204
Rodrigo Rocha <rodrigorgs@gmail.com>
2018-06-29 10:04:25 -0300

    Move ILP Game para https://rodrigorgs.github.io/ilpgame/

--- mata37/sdl/compila.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ $@ -std=c++11 `sdl2-config --cflags --libs` -lSDL2_ttf -lSDL2_image -lSDL2_mixer
--- mata37/sdl/tut-arquivo.h
+++
@@ -1,3 +0,0 @@
-extern SDL_Surface *img;
-extern int playerx, playery;
-extern int velocidade;
