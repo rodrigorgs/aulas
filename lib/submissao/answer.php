224b9bf4d9a97e9584e0e91aed1fc7911c2cbfa6
Will19 <wiliamgomesc19@gmail.com>
2020-04-08 15:59:05 -0300

    Altera pagina de respostas para que ela possa exibir a resposta correta

--- lib/submissao/answer.php
+++ lib/submissao/answer.php
@@ -2,15 +2,17 @@
 
 	session_start();
 		
-	$nome = (STRING)$_GET["name"];
+	$nome = $_GET["name"];
 
-	echo $nome."<br>";
+	//echo $nome."<br>";
 
-	$button_index = (INT)$_GET["questao"];
+	$button_index = $_GET["questao"];
 
-	echo $button_index."<br>";
+	//echo $button_index."<br>";
 
-	$apostila = $_GET["apostila"];
+	//$apostila = $_GET["apostila"];
+	
+	$apostila = "unica";
 
 	$conn = mysqli_connect("localhost", "root", "123456", "mata56");
 
@@ -22,17 +24,17 @@
 	
   	mysqli_query($conn, "SET NAMES 'utf8'");
 	
-	$query = "SELECT answers FROM resposta WHERE nome = ? AND button_index = ? AND apostila = ?;";
+	$query = "select answers from resposta where (nome = ? and button_index = ? and apostila = ?);";
 
-	echo $query."<br>";
+	//echo $query."<br>";
 
 	$sql = $conn->prepare($query);
 	
-	$sql->bind_param('sis', $nome, $button_index, $apostila);
+	$sql->bind_param("sis", $nome, $button_index, $apostila);
+	
+	$sql->execute();
 
-	$result = $sql->execute();
-
-	echo $result."<br>";
+	$result = $sql->get_result();
 
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
