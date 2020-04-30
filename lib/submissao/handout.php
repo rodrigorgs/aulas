648a9960b55d6a70035a0522d888edba055ea539
Will19 <wiliamgomesc19@gmail.com>
2020-04-30 16:55:37 -0300

    Retira testes_ok e testes_total e adiciona results no lugar deles

--- lib/submissao/handout.php
+++ lib/submissao/handout.php
@@ -7,6 +7,7 @@
 	//$apostila = "unica";
 
 	function defineColor($parameter){
+		//echo $parameter. "<br>";
 		if($parameter < 5.0){
 			return "red";
 		}
@@ -30,7 +31,13 @@
   	}
   	mysqli_query($conn, "SET NAMES 'utf8'");
 
-	$query = "select matricula, nome, button_index, testes_ok, testes_total from resposta where (apostila = ? && button_index > 0) order by nome, button_index";
+	$query = "select matricula, nome, button_index, results from resposta where (apostila = ? && button_index > 0) order by nome, button_index";
+
+	$zeroquery = "INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', ?, ?, ?, ?, ?)"; 
+
+	$jsonpadrao = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;\\n\"]}";
+
+	$resultspadrao = "{\"results\": [{\"ok\": 3, \"total\": 10}]}";
 
 	//echo $query;
 
@@ -45,6 +52,28 @@
 	$result_2 = $sql_2->get_result();
 	$num_linhas = mysqli_fetch_array($result_2, MYSQLI_ASSOC);
 	$total_rows = $num_linhas['max(button_index)'];
+
+	$questoestotais = array();
+	$questoestotais[0] = 10;
+
+	$contador = 1;
+	while($contador <= $total_rows){
+		$totalparc = 0;
+		$sql_4 = $conn->prepare("select results from resposta where button_index = ?");
+		$sql_4->bind_param("i", $contador);
+		$sql_4->execute();
+		$result_4 = $sql_4->get_result();
+		while($t = mysqli_fetch_array($result_4, MYSQLI_ASSOC)){
+			$val_qparcial = $t["results"];
+			$val_qtotal = json_decode($val_qparcial, true);
+			$val_q = $val_qtotal["results"][0]["total"];
+			if($val_q > $totalparc){
+				$totalparc = $val_q;
+			}
+		}
+		$questoestotais[$contador] = $totalparc;
+		$contador = $contador + 1;
+	}
 
 	echo "<h1>Atividades</h1>";
 
@@ -65,13 +94,17 @@
 	$matricula = $row['matricula'];
 	$nome = $row['nome'];
 	$button_index = $row['button_index'];
-	$testes_ok = $row['testes_ok'];
-	$testes_total = $row['testes_total'];
+	$results = $row['results'];
+	//echo $results . "<br>";
+	$testes = json_decode($results, true);
+	//var_dump($testes["results"][0]["ok"]) . "<br>";
+	$testes_ok = $testes["results"][0]["ok"];
+	$testes_total = $testes["results"][0]["total"];
 	$testes_corretos = $testes_ok / $testes_total * 100;
 	echo "<tr><td style = 'text-align: center;'>".$matricula."</td><td style = 'text-align: center;'>".$nome."</td>";
 	if ($button_index == 1){
 		echo "<td style = 'text-align: center; color:".
-			defineColor($testes_corretos).
+			defineColor($testes_corretos/10).
 			";'>".
 			"<a href = 'answer.php?apostila=".$apostila."&mat=".$matricula."&questao=".$button_index."'>".$testes_ok."</a>"." (".
 			number_format((float)$testes_corretos, 2, '.', '')."%)".
@@ -79,6 +112,10 @@
 			//echo $button_index." first part one <br>";
 	}
 	else{
+		$resultspadrao = "{\"results\": [{\"ok\": 0, \"total\": ". $questoestotais[1] ."}]}";
+		$sql_3 = $conn->prepare($zeroquery);
+		$sql_3->bind_param("ssiss", $nome, $matricula, 1, $jsonpadrao, $resultspadrao);
+		$sql_3->execute();
 		echo "<td style = 'text-align: center; color:red'>"."0 (".number_format(0, 2, '.', '')."%)"."</td>";
 		//echo "first part two <br>";
 	}				
@@ -88,15 +125,23 @@
 			$button_index = $row['button_index'];
 			if ($button_index > $num_rows){
 				while ($num_rows < $button_index){
+					$resultspadrao = "{\"results\": [{\"ok\": 0, \"total\": ". $questoestotais[$num_rows] ."}]}";
+					$sql_3 = $conn->prepare($zeroquery);
+					$sql_3->bind_param("ssiss", $nome, $matricula, $num_rows, $jsonpadrao, $resultspadrao);
+					$sql_3->execute();
 					echo "<td style = 'text-align: center; color:red;'>"."0 (".number_format(0, 2, '.', '')."%)"."</td>";
 					//echo $num_rows." ".$button_index." second part one <br>";
 					$num_rows++;
 				}
-				$testes_ok = $row['testes_ok'];
-				$testes_total = $row['testes_total'];
+				$results = $row['results'];
+				//echo $results . "<br>";
+				$testes = json_decode($results, true);
+				//var_dump($testes["results"][0]["ok"]) . "<br>";
+				$testes_ok = $testes["results"][0]["ok"];
+				$testes_total = $testes["results"][0]["total"];
 				$testes_corretos = $testes_ok / $testes_total * 100;
 				echo "<td style = 'text-align: center; color:".
-				defineColor($testes_corretos).
+				defineColor($testes_corretos/10).
 				";'>".
 				"<a href = 'answer.php?apostila=".$apostila."&mat=".$matricula."&questao=".$button_index."'>".$testes_ok."</a>"." (".
 				number_format((float)$testes_corretos, 2, '.', '')."%)".
@@ -104,11 +149,16 @@
 				//echo $button_index." second part two <br>";
 			}
 			else{
-				$testes_ok = $row['testes_ok'];
-				$testes_total = $row['testes_total'];
+				$results = $row['results'];
+				//echo $results . "<br>";
+				$testes = json_decode($results, true);
+				//var_dump($testes["results"][0]["total"]) . "<br>";
+				$testes_ok = $testes["results"][0]["ok"];
+				$testes_total = $testes["results"][0]["total"];
+				//echo $testes_ok . "<br>" . $testes_total . "<br>";
 				$testes_corretos = $testes_ok / $testes_total * 100;
 				echo "<td style = 'text-align: center; color:".
-				defineColor($testes_corretos).
+				defineColor($testes_corretos/10).
 				";'>".
 				"<a href = 'answer.php?apostila=".$apostila."&mat=".$matricula."&questao=".$button_index."'>".$testes_ok."</a>"." (".
 				number_format((float)$testes_corretos, 2, '.', '')."%)";
@@ -120,6 +170,10 @@
 			$num_rows++;
 			if ($num_rows <= $total_rows){
 				while ($num_rows <= $total_rows){
+					$resultspadrao = "{\"results\": [{\"ok\": 0, \"total\": ". $questoestotais[$num_rows] ."}]}";
+					$sql_3 = $conn->prepare($zeroquery);
+					$sql_3->bind_param("ssiss", $nome, $matricula, $num_rows, $jsonpadrao, $resultspadrao);
+					$sql_3->execute();
 					echo "<td style = 'text-align: center; color:red;'>"."0 (".number_format(0, 2, '.', '')."%)"."</td>";
 					$num_rows++;
 					//echo $num_rows." third part one <br>";
@@ -130,13 +184,17 @@
 			$matricula = $row['matricula'];
 			$nome = $row['nome'];
 			$button_index = $row['button_index'];
-			$testes_ok = $row['testes_ok'];
-			$testes_total = $row['testes_total'];
+			$results = $row['results'];
+			//echo $results . "<br>";
+			$testes = json_decode($results, true);
+			//var_dump($testes["results"][0]["ok"]) . "<br>";
+			$testes_ok = $testes["results"][0]["ok"];
+			$testes_total = $testes["results"][0]["total"];
 			$testes_corretos = $testes_ok / $testes_total * 100;
 			echo "<tr><td>".$matricula."</td><td>".$nome."</td>";
 			if ($button_index == 1){
 				echo "<td style = 'text-align: center; color:".
-					defineColor($testes_corretos).
+					defineColor($testes_corretos/10).
 					";'>".
 					"<a href = 'answer.php?apostila=".$apostila."&mat=".$matricula."&questao=".$button_index."'>".$testes_ok."</a>"." (".
 					number_format((float)$testes_corretos, 2, '.', '')."%)";
@@ -145,15 +203,24 @@
 			}
 			else{
 					while ($num_rows < $button_index){
+						$resultspadrao = "{\"results\": [{\"ok\": 0, \"total\": ". $questoestotais[$num_rows] ."}]}";
+						//echo $resultspadrao . "<br>";
+						$sql_3 = $conn->prepare($zeroquery);
+						$sql_3->bind_param("ssiss", $nome, $matricula, $num_rows, $jsonpadrao, $resultspadrao);
+						$sql_3->execute();
 						echo "<td style = 'text-align: center; color:red;'>"."0 (".number_format(0, 2, '.', '')."%)"."</td>";
 						//echo $num_rows." ".$button_index." third part three <br>";
 						$num_rows++;
 					}
-					$testes_ok = $row['testes_ok'];
-					$testes_total = $row['testes_total'];
+					$results = $row['results'];
+					//echo $results . "<br>";
+					$testes = json_decode($results, true);
+					//var_dump($testes["results"][0]["ok"]) . "<br>";
+					$testes_ok = $testes["results"][0]["ok"];
+					$testes_total = $testes["results"][0]["total"];
 					$testes_corretos = $testes_ok / $testes_total * 100;
 					echo "<td style = 'text-align: center; color:".
-					defineColor($testes_corretos).
+					defineColor($testes_corretos/10).
 					";'>".
 					"<a href = 'answer.php?apostila=".$apostila."&mat=".$matricula."&questao=".$button_index."'>".$testes_ok."</a>"." (".
 					number_format((float)$testes_corretos, 2, '.', '')."%)".
@@ -165,6 +232,10 @@
 	$num_rows++;
 	if ($num_rows <= $total_rows){
 		while ($num_rows <= $total_rows){
+			$resultspadrao = "{\"results\": [{\"ok\": 0, \"total\": ". $questoestotais[$num_rows] ."}]}";
+			$sql_3 = $conn->prepare($zeroquery);
+			$sql_3->bind_param("ssiss", $nome, $matricula, $num_rows, $jsonpadrao, $resultspadrao);
+			$sql_3->execute();
 			echo "<td style = 'text-align: center; color:red'>"."0 (".number_format(0, 2, '.', '')."%)"."</td>";
 			$num_rows++;
 			//echo $num_rows." fourth part one <br>";
--- lib/submissao/tests.php
+++ lib/submissao/tests.php
@@ -5,6 +5,8 @@
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 15 (area '(5 3)))\\n;(teste 20 (area '(4 5)))\\n;(teste 0 (area '(2 0)))\\n\"]}";
 
+	$val = "{\"results\": [{\"ok\": 3, \"total\": 10}]}";
+
 	/* echo "INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 1, '".json_encode($value)."', 3, 10); <br>";
 
 	var_dump(json_encode($value, true));
@@ -28,92 +30,107 @@
 							matricula VARCHAR(20),
 					  		button_index INT,
 							answers TEXT,
-							testes_ok INT,
-							testes_total INT
+							results TEXT
 						);");
 
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 1, ? , 3, 10);");
-	$sql->bind_param("s", $value);
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 1, ? , ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 4 (area '(2 2)))\\n;(teste 10 (area '(5 2)))\\n;(teste 6 (area '(3 2)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 2, ?, 3, 6);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 3, \"total\": 6}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 2, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 100 (area '(2 50)))\\n;(teste 25 (area '(5 5)))\\n;(teste 18 (area '(9 2)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 1, ?, 5, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 5, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 1, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 13 (area '(13 1)))\\n;(teste 144 (area '(12 12)))\\n;(teste 169 (area '(13 13)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 2, ?, 6, 6);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 6, \"total\": 6}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 2, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 	
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 90 (area '(15 6)))\\n;(teste 310 (area '(31 10)))\\n;(teste 5 (area '(1 5)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 1, ?, 8, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 8, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 1, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 1 (area '(1 1)))\\n;(teste 30 (area '(15 2)))\\n;(teste 8 (area '(4 2)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 2, ?, 1, 6);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 1, \"total\": 6}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 2, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 40 (area '(2 20)))\\n;(teste 90 (area '(3 30)))\\n;(teste 70 (area '(14 5)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 4, ?, 4, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 4, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 4, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 200 (area '(2 100)))\\n;(teste 200 (area '(25 8)))\\n;(teste 200 (area '(50 4)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 3, ?, 15, 15);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 15, \"total\": 15}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 3, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 350 (area '(7 50)))\\n;(teste 490 (area '(7 70)))\\n;(teste 280 (area '(7 40)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 3, ?, 10, 15);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 15}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 3, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 9 (area '(1 9)))\\n;(teste 11 (area '(11 1)))\\n;(teste 19 (area '(19 1)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 4, ?, 7, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 7, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 4, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 55 (area '(5 11)))\\n;(teste 33 (area '(3 11)))\\n;(teste 99 (area '(9 11)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 3, ?, 3, 15);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 3, \"total\": 15}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 3, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"; se quiser, pode definir funções auxiliares\\n(define (area medidas) \\n 'implementeAFuncao)\\n\\n(teste 12 (area '(3 4)))\\n; Crie no mínimo 2 testes adicionais\\n;(teste 60 (area '(12 5)))\\n;(teste 48 (area '(4 12)))\\n;(teste 72 (area '(6 12)))\\n\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 1, ?, 9, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 9, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 1, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"Sicrano\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 0, ?, 10, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrano', '0988907654', 0, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"Beltrano\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 0, ?, 10, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Beltrano', '0987654321', 0, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"Fulano\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 0, ?, 10, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulano', '1234567890', 0, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"Sicrana\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 0, ?, 10, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Sicrana', '1233214567', 0, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	$value = "{\"answers\":[\"Fulana\"]}";
-	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, testes_ok, testes_total) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 0, ?, 10, 10);");
-	$sql->bind_param("s", $value);
+	$val = "{\"results\": [{\"ok\": 10, \"total\": 10}]}";
+	$sql = $conn->prepare("INSERT INTO resposta (timestamp, apostila, nome, matricula, button_index, answers, results) VALUES (NOW(), 'unica', 'Fulana', '1238634712', 0, ?, ?);");
+	$sql->bind_param("ss", $value, $val);
 	$result = $sql->execute();
 
 	echo "Tudo foi impresso";
