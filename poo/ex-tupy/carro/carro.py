368bac8e5a894f64d291d6cfb2b1d739f3ed148f
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-05-15 18:19:02 -0300

    .

--- poo/ex-tupy/carro/carro.py
+++ poo/ex-tupy/carro/carro.py
@@ -1,33 +1,33 @@
 from tupy import *
 
-class Carro(Image):
+class Carro(BaseImage):
     def __init__(self):
-        self.x = 50
-        self.y = 200
-        self.__velocidade = 0
+        self._x = 50
+        self._y = 200
+        self._velocidade = 0
         # visibilidade / acesso ---- sugestão para o programador
     
     def get_velocidade(self):
-        return self.__velocidade
+        return self._velocidade
 
     def set_velocidade(self, v):
         if v >= 0 and v <= 40:
-            self.__velocidade = v
+            self._velocidade = v
 
     def acelerar(self):
-        self.__velocidade += 5
-        if self.__velocidade > 40:
-            self.__velocidade = 40
+        self._velocidade += 5
+        if self._velocidade > 40:
+            self._velocidade = 40
     
-    def freiar(self):
-        self.__velocidade -= 5
-        if self.__velocidade < 0:
-            self.__velocidade = 0
+    def frear(self):
+        self._velocidade -= 5
+        if self._velocidade < 0:
+            self._velocidade = 0
 
     def update(self):
-        self.x += self.__velocidade
-        if self.x > 850:
-            self.x = -50
+        self._x += self._velocidade
+        if self._x > 850:
+            self._x = -50
 
 carro = Carro()
 
