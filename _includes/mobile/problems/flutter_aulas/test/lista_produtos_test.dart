4cff33c8a4364983c5e7caa71451d175c20d2aab
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-10-20 19:02:37 -0300

    .

---
+++ _includes/mobile/problems/flutter_aulas/test/lista_produtos_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/lista_produtos.dart';
+
+void main() {
+  group('ListaCompras', () {
+    testWidgets('Deve exibir a lista de produtos', (WidgetTester tester) async {
+      final produtos = [
+        Produto("Arroz", 10.0),
+        Produto("Feijão", 5.0),
+        Produto("Macarrão", 3.0),
+        Produto("Carne", 20.0),
+      ];
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: ListaCompras(produtos: produtos),
+          ),
+        ),
+      );
+
+      expect(find.text('Arroz'), findsOneWidget);
+      expect(find.text('Feijão'), findsOneWidget);
+      expect(find.text('Macarrão'), findsOneWidget);
+      expect(find.text('Carne'), findsOneWidget);
+    });
+
+    testWidgets('Deve exibir o preço de cada produto',
+        (WidgetTester tester) async {
+      final produtos = [
+        Produto("Arroz", 10.0),
+        Produto("Feijão", 5.0),
+        Produto("Macarrão", 3.0),
+        Produto("Carne", 20.0),
+      ];
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: ListaCompras(produtos: produtos),
+          ),
+        ),
+      );
+
+      expect(find.text('10.0 reais'), findsOneWidget);
+      expect(find.text('5.0 reais'), findsOneWidget);
+      expect(find.text('3.0 reais'), findsOneWidget);
+      expect(find.text('20.0 reais'), findsOneWidget);
+    });
+  });
+}
---
+++ mobile/ex/20232/03-flutter-stateless.md
@@ -0,0 +1,8 @@
+---
+layout: triple-page
+title: "Exercícios: Flutter stateless"
+features: [code, flutter, uml]
+---
+
+{% include mobile/problems/flutter_aulas/md/lista_produtos.md %}
+{% include mobile/problems/flutter_aulas/md/navega.md %}
